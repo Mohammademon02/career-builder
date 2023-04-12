87f55599284d986ce0c218b19d77b4af60896c22
mohammademon02 <mohammademon0207@gmail.com>
2023-04-12 22:57:22 +0600

    some error fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
                 <h2 className='fs-2 fw-bold text-center mt-5'>Job Category List</h2>
                 <p className='text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
 
-                <div className='d-flex align-items-center justify-content-between mt-5'>
+                <div className='row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 '>
                     {
                         jobCategory.map(category => <JobCategory
                             key={category.id}
