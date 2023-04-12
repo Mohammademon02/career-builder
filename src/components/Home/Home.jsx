f7e0793a8e26dbf249e320367d48e983b3f5c902
mohammademon02 <mohammademon0207@gmail.com>
2023-04-12 06:30:51 +0600

    job category section completed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Image from '../../assets/All Images/P3OLGJ1 copy 1.png'
+import { useLoaderData } from 'react-router-dom';
+import JobCategory from '../JobCategory/JobCategory';
 
 const Home = () => {
+
+    const jobCategory = useLoaderData();
     return (
-        <div>
-            <div className='container d-flex  align-items-center  justify-content-between'>
+        <div className='container'>
+
+            <div className=' d-flex  align-items-center  justify-content-between'>
                 <div>
                     <h1 className='fs-1 fw-bold'>One Step <br /> Closer To Your <br /> <span className='text-primary'>Dream Job</span> </h1>
                     <p> Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</p>
@@ -12,6 +17,20 @@ const Home = () => {
                 </div>
                 <div>
                     <img className='img-fluid' src={Image} alt="" />
+                </div>
+            </div>
+
+            <div>
+                <h2 className='fs-2 fw-bold text-center mt-5'>Job Category List</h2>
+                <p className='text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+
+                <div className='d-flex align-items-center justify-content-between mt-5'>
+                    {
+                        jobCategory.map(category => <JobCategory
+                            key={category.id}
+                            category={category}
+                        ></JobCategory>)
+                    }
                 </div>
             </div>
         </div>
---
+++ src/components/JobCategory/JobCategory.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const JobCategory = ({category}) => {
+   
+    return (
+        <div className='border p-3 text-start rounded'>
+            <img src={category.categoryLogo} alt="" />
+            <h4>{category.categoryName}</h4>
+            <p>{category.availableJobs}</p>
+        </div>
+    );
+};
+
+export default JobCategory;
